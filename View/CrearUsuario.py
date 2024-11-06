1196433ed0648557b17c8de7d26f97e34ca75747
diablaxus <frank.berrio@correounivalle.edu.co>
2024-11-06 12:18:20 -0500

    Corrección iconos

--- View/CrearUsuario.py
+++ View/CrearUsuario.py
@@ -41,12 +41,12 @@ class CrearUsuario():
         self.txtRol=tk.Entry(self.ventana)
         self.txtRol.place(x=160,y=235,width=150,height=25)
 
-        self.icono_guardar=tk.PhotoImage(file=r"src\icons\disk.png")
+        self.icono_guardar=tk.PhotoImage(file=r"MVC\src\icons\disk.png")
         self.btnGuardar=tk.Button(self.ventana,text="Guardar",image=self.icono_guardar,compound="left")
         self.btnGuardar.place(x=70,y=310,width=80,height=25)
         self.btnGuardar.bind("<Button-1>",self.GuardarUsuario)
         
-        self.icono_limpiar=tk.PhotoImage(file=r"src\icons\textfield_delete.png")
+        self.icono_limpiar=tk.PhotoImage(file=r"MVC\src\icons\bin_closed.png")
         self.btnLimpiar=tk.Button(self.ventana,text="Limpiar",image=self.icono_limpiar,compound="left")
         self.btnLimpiar.place(x=195,y=310,width=80,height=25)
 
